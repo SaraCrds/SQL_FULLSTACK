55dbcd9fdb0b60c9aa2e3c244b36e5714cef2b3b
SaraCrds <sara.decastro@outlook.com>
2023-04-13 16:13:53 +0200

    update 1304

--- filrouge_thedistrict.sql
+++ filrouge_thedistrict.sql
@@ -31,11 +31,42 @@ ORDER BY nmbr_vendu DESC;
 
 -- Le plat le plus rémunérateur (pas finis)
 
-SELECT plat.libelle AS nom_plat, commande.quantite*plat.prix AS total
+SELECT plat.libelle AS nom_plat, MAX(commande.quantite*plat.prix) AS total
 FROM plat
 JOIN commande
-ON commande.id_plat = plat.id
-GROUP BY nom_plat  
-ORDER BY `total` DESC;
+ON commande.id_plat = plat.id;
 
 -- Liste des clients et le chiffre d'affaire généré par client (par ordre décroissant)
+
+SELECT nom_client, total AS chiffre_affaire FROM `commande` ORDER BY chiffre_affaire DESC;
+
+-------- MODIFICATION
+-- Ecrivez une requête permettant de supprimer les plats non actif de la base de données
+
+DELETE FROM plat WHERE active = 'No';
+
+-- Ecrivez une requête permettant de supprimer les commandes avec le statut livré
+
+DELETE FROM commande WHERE etat = 'Livrée';
+
+-- Ecrivez un script sql permettant d'ajouter une nouvelle catégorie et un plat dans cette nouvelle catégorie.
+
+INSERT INTO `categorie`(`libelle`, `image`, `active`) VALUES (1, 2, 3)
+
+INSERT INTO `plat`(`libelle`, `description`, `prix`, `image`, `id_categorie`, `active`) 
+VALUES (1, 2, 3, 4, LAST_INSERT_ID(), 6);
+
+-- c'est pas du tout ça BREF je reviens dessus plus tard
+
+INSERT INTO categorie (libelle, image, active)
+  VALUES('test', 'test', 'test');
+INSERT INTO plat (libelle, description, prix, image, id_categorie, active) 
+  VALUES('test', 'test', 12, 'test', LAST_INSERT_ID(),'test');
+
+-- Ecrivez une requête permettant d'augmenter de 10% le prix des plats de la catégorie 'Pizza'
+
+UPDATE plat SET prix = prix * 1.1 WHERE id_categorie = (SELECT id FROM categorie WHERE libelle = 'Pizza');
+
+
+
+
